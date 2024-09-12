27ebf3e2863744181852b2bfb0334a60c8db0d7e
aasiflm10 <aa6125405@gmail.com>
2024-09-12 23:26:16 +0530

    wallet adapter added

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,31 +1,48 @@
+'use client'
 import AnimatedBackground from '@/components/core/animated-background';
+import { useState } from 'react';
 
 export function AnimatedTabsHover() {
   const TABS = ['Wallet', 'Airdrop', 'SignMessage', 'SendSOL'];
-
+//   const SELECTED = ['Wallet', 'Airdrop', 'SignMessage', 'SendSOL']
+  const [selected, setSelected] = useState('Wallet');
   return (
-    <div className='flex flex-row justify-center space-x-8'>
-      <AnimatedBackground
-        defaultValue={TABS[0]}
-        className='rounded-lg bg-zinc-100 dark:bg-zinc-800'
-        transition={{
-          type: 'spring',
-          bounce: 0.2,
-          duration: 0.3,
-        }}
-        enableHover
-      >
-        {TABS.map((tab, index) => (
-          <button
-            key={index}
-            data-id={tab}
-            type='button'
-            className=' text-2xl px-2 py-0.5 text-zinc-600 transition-colors duration-300 hover:text-zinc-950 dark:text-zinc-400 dark:hover:text-zinc-50'
-          >
-            {tab}
-          </button>
-        ))}
-      </AnimatedBackground>
+    <div>
+        <div className='flex flex-row justify-center space-x-8'>
+        <AnimatedBackground
+            defaultValue={TABS[0]}
+            className='rounded-lg bg-zinc-100 dark:bg-zinc-800'
+            transition={{
+            type: 'spring',
+            bounce: 0.2,
+            duration: 0.3,
+            }}
+            enableHover
+        >
+            {TABS.map((tab, index) => (
+            <button
+                key={index}
+                data-id={tab}
+                type='button'
+                className=' text-2xl px-2 py-0.5 text-zinc-600 transition-colors duration-300 hover:text-zinc-950 dark:text-zinc-400 dark:hover:text-zinc-50'
+                onClick={()=>{
+                    setSelected(tab)
+                    console.log(selected);
+                }}
+            >
+                {tab}
+            </button>
+            ))}
+        </AnimatedBackground>
+        </div>
+        
+        <div>
+        {selected === 'SignMessage' ? (
+            <div>
+                hi there
+            </div>
+        ) : null}
+        </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,29 @@
-import Image from "next/image";
+'use client'
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletDisconnectButton, WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { AnimatedTabsHover } from "./components/NavBar";
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 export default function Home() {
   return (
-    <div>
-      <AnimatedTabsHover/>
-    </div>
-      );
+    <ConnectionProvider endpoint={"http://solana.devnet.com"}>
+        <WalletProvider wallets={[]} autoConnect>
+            <WalletModalProvider>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                padding: 20
+              }}>
+                <WalletMultiButton />
+                <WalletDisconnectButton />
+              </div>
+                <AnimatedTabsHover/>
+            </WalletModalProvider>
+        </WalletProvider>
+    </ConnectionProvider>
+);
+
 }
